04e8729646b7522b6fd124a0ab75098625decc25
cristiana.goulao <cristiana.goulao@babelgroup.com>
2026-06-02 18:04:18 +0100

    Initial Spring Boot and Camunda setup

---
+++ src/test/java/com/cristiana/camundademo/CamundaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristiana.camundademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamundaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
